b1980206ac9861d8a315833c1853686eb9655df4
ikillfairies <ikillfairies@users.noreply.github.com>
2016-12-03 20:01:46 -0500

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $('.navLink').mouseover(function() { 
+    $(this).stop().animate({color: '#F5F5F5'}, 150); });
+  $('.navLink').mouseout(function() { 
+    $(this).stop().animate({color: '#B0B0B0'}, 300); });
+  $('.active').unbind();
+
+});
