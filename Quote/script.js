d6a07bc90a266eee3cb9fe32e08ce16560197835
ikillfairies <ikillfairies@users.noreply.github.com>
2017-01-11 12:31:12 -0500

    Update script.js

--- Quote/script.js
+++ Quote/script.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
     $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
   });
+  $.ajaxSetup({
+    // Disable caching of AJAX responses
+    cache: false
+  });
 });
