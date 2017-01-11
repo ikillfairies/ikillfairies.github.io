0dcaa5342e5e2da167fe4414d9f2c22474414b81
ikillfairies <ikillfairies@users.noreply.github.com>
2017-01-11 18:23:53 -0500

    Add files via upload

--- Quote/script.js
+++ Quote/script.js
@@ -1,26 +1,38 @@
+var title;
+var content;
+var source;
+
 $(document).ready(function() {
-
-  $('#getQuote').on('click', function(e) {
-    e.preventDefault();
-    $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
-      success: function(data) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quoteTitle').text(post.title);
-        $('#quoteContent').html(post.content);
-        // If the Source is available, use it. Otherwise hide it.
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quoteSource').html('Source: ' + post.custom_meta.Source);
-        } else {
-          $('#quoteSource').text('');
-        }
-      },
-      cache: false
-    });
-  });
-
+  getQuote();
   $('#getQuote').click();
+});
 
-  $('#quote').animate({'opacity': 1}, 350);
+function getQuote() {
+  $.ajax( {
+    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+    success: function(data) {
+      var post = data[0]
+      title = post.title;
+      content = post.content;
+      // If the Source is available, use it. Otherwise hide it.
+      if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+        source = post.custom_meta.Source;
+      } else {
+        source = '';
+      }
+    },
+    cache: false
+  });
+}
 
+$('#getQuote').on('click', function(e) {
+  $('#quote').animate({'opacity': 0}, 100);
+  setTimeout(function() { 
+    $('#quoteTitle').text(title);
+    $('#quoteContent').html(content);
+    $('#quoteSource').html('Source: ' + source);
+  }, 100);
+  $('#quote').delay(125).animate({'opacity': 1}, 200);
+  getQuote();
+  e.preventDefault();
 });
