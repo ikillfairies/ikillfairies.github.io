ed6277b2d75235afa5d1b25e2afebd34d4199c51
ikillfairies <ikillfairies@users.noreply.github.com>
2017-01-11 18:28:31 -0500

    Add files via upload

--- Quote/script.js
+++ Quote/script.js
@@ -3,35 +3,41 @@ var content;
 var source;
 
 $(document).ready(function() {
-  getQuote();
-  $('#getQuote').click();
-});
 
-function getQuote() {
-  $.ajax( {
-    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
-    success: function(data) {
-      var post = data[0]
-      title = post.title;
-      content = post.content;
-      // If the Source is available, use it. Otherwise hide it.
-      if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-        source = post.custom_meta.Source;
-      } else {
-        source = 'not available';
-      }
-    },
-    cache: false
+  function getQuote() {
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+      success: function(data) {
+        var post = data[0]
+        title = post.title;
+        content = post.content;
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          source = post.custom_meta.Source;
+        } else {
+          source = 'not available';
+        }
+      },
+      cache: false
+    });
+  };
+
+  getQuote();
+
+  $('#getQuote').on('click', function() {
+    $('#quote').animate({'opacity': 0}, 75);
+    setTimeout(function() { 
+      $('#quoteTitle').html(title);
+      $('#quoteContent').html(content);
+      $('#quoteSource').html('Source: ' + source);
+    }, 75);
+    $('#quote').delay(75).animate({'opacity': 1}, 150);
+    getQuote();
   });
-}
 
-$('#getQuote').on('click', function() {
-  $('#quote').animate({'opacity': 0}, 75);
-  setTimeout(function() { 
-    $('#quoteTitle').html(title);
-    $('#quoteContent').html(content);
-    $('#quoteSource').html('Source: ' + source);
-  }, 75);
-  $('#quote').delay(75).animate({'opacity': 1}, 150);
-  getQuote();
+  $('#getQuote').click();
+  
 });
+
+
+
