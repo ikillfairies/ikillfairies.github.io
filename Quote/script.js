225c8f5b8826c78c6f297aa694ed29c3eb4a0d71
ikillfairies <ikillfairies@users.noreply.github.com>
2017-01-11 12:40:10 -0500

    Add files via upload

--- Quote/script.js
+++ Quote/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-    $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-  });
-  $.ajaxSetup({
-    // Disable caching of AJAX responses
-    cache: false
+  $('#getQuote').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $('#quoteTitle').text(post.title);
+        $('#quoteContent').html(post.content);
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quoteSource').html('Source:' + post.custom_meta.Source);
+        } else {
+          $('#quoteSource').text('');
+        }
+      },
+      cache: false
+    });
   });
 });
