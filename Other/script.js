07bd42ae975a590f3a6bb6ab14213daf62da52fa
Jeffrey Yan <ikillfairies@users.noreply.github.com>
2018-08-17 15:21:58 -0400

    Delete script.js

--- Other/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.navButton').mouseover(function() { 
-    $(this).stop().animate({color: '#F5F5F5'}, 150); });
-  $('.navButton').mouseout(function() { 
-    $(this).stop().animate({color: '#B0B0B0'}, 300); });
-
-});
